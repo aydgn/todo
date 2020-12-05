4cf5bc115030482e87b4282c0388b6b60b872601
Aydoğan <aydogan.gokberk@gmail.com>
2020-12-06 01:20:28 +0300

    Merhaba isim alanı eklendi

--- src/components/Zaman.js
+++ src/components/Zaman.js
@@ -1,17 +1,41 @@
-const Zaman = () => {
-  const time = () => {
-    const e = new Date();
-    const tarih = e.toLocaleDateString("tr-TR");
-    const saat = e.toLocaleTimeString("tr-TR", {
+import React, { useState, useEffect } from "react";
+
+const ZamanBar = () => {
+  // Zamanı göstermek için
+  const Time = () => {
+    const d = new Date();
+    const tarih = d.toLocaleDateString("tr-TR");
+    const saat = d.toLocaleTimeString("tr-TR", {
       hour: "2-digit",
       minute: "2-digit",
     });
-    const gun = e.toLocaleString("tr-TR", { weekday: "long" });
-
-    const hepsi = `${tarih} | ${saat} | ${gun}`;
+    const gun = d.toLocaleString("tr-TR", { weekday: "long" });
+    const hepsi = ` ${tarih} | ${saat} | ${gun}`;
     return hepsi;
   };
 
-  return <div>{time()}</div>;
+  const defaultName = () => {
+    return window.localStorage.getItem("name") || "(ismini girmek için tıkla)";
+    // default name localstorage'da varsa getir yoksa yazıyı göster.
+  };
+  const [name, setname] = useState(defaultName);
+
+  useEffect(() => {
+    document.title = `${name}'s To Do List`;
+    localStorage.setItem("name", name);
+  });
+
+  return (
+    <div>
+      <span
+        onClick={() => {
+          setname(prompt("Adın ne şekerlik?"));
+        }}
+      >
+        Merhaba, {name}.
+      </span>
+      {Time()}
+    </div>
+  );
 };
-export default Zaman;
+export default ZamanBar;
