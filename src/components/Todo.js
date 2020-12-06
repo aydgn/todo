94699d25545b67b9e9ddbbcb0c0e2f55fb6d3cbf
Aydoğan <aydogan.gokberk@gmail.com>
2020-12-06 18:01:16 +0300

    button size

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-const Todo = ({ text }) => {
+const Todo = ({ text, todo, todos, setTodos }) => {
+  const completeHandler = () => {};
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+
+    console.log(todo);
+  };
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
-      <button className="tamamla">✔️</button>
-      <button className="sil">❌</button>
+      <button className="complete" onClick={completeHandler}>
+        ✔️
+      </button>
+      <button className="delete" onClick={deleteHandler}>
+        🗑️
+      </button>
     </div>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Todo from "./Todo";
 
-function TodoList({ todos }) {
+function TodoList({ todos, setTodos }) {
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo) => (
-          <Todo key={todo.id} text={todo.text} id={todo.id} />
+          <Todo
+            key={todo.id}
+            text={todo.text}
+            id={todo.id}
+            setTodos={setTodos}
+            todo={todo}
+          />
         ))}
       </ul>
     </div>
