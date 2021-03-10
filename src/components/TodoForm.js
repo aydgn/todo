ca3c22eebaf1f6dfcd24b92d90fb3f461c712a1d
Aydoğan Gökberk <aydogan.gokberk@gmail.com>
2021-03-10 20:18:07 +0300

    package.json update

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,47 +1,47 @@
 import React from "react";
 
 function TodoForm({ inputText, setInputText, todos, setTodos }) {
-  const inputTextHandler = (e) => {
-    setInputText(e.target.value);
-  };
-  const submitButtonHandler = (e) => {
-    e.preventDefault();
-    setTodos([
-      ...todos,
-      {
-        text: inputText,
-        completed: false,
-        id: Math.floor(Math.random() * 1000),
-      },
-    ]);
-    setInputText(""); // clears input value after submit
+	const inputTextHandler = (e) => {
+		setInputText(e.target.value);
+	};
+	const submitButtonHandler = (e) => {
+		e.preventDefault();
+		setTodos([
+			...todos,
+			{
+				text: inputText,
+				completed: false,
+				id: Math.floor(Math.random() * 1000),
+			},
+		]);
+		setInputText(""); // clears input value after submit
 
-    // save local storage
-  };
+		// *TODO* save local storage
+	};
 
-  return (
-    <div className="container-fluid">
-      <form className="d-flex mt-3">
-        <input
-          type="text"
-          name="todo-input"
-          className="form-control ml-2"
-          placeholder=""
-          onChange={inputTextHandler}
-          autoFocus
-          autoComplete="off"
-          value={inputText}
-        />
-        <button
-          type="submit"
-          className="btn btn-dark btn-lg mx-2 px-3"
-          onClick={submitButtonHandler}
-        >
-          +
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="container-fluid">
+			<form className="d-flex mt-3">
+				<input
+					type="text"
+					name="todo-input"
+					className="form-control ml-2"
+					placeholder=""
+					onChange={inputTextHandler}
+					autoFocus
+					autoComplete="off"
+					value={inputText}
+				/>
+				<button
+					type="submit"
+					className="btn btn-dark btn-lg mx-2 px-3"
+					onClick={submitButtonHandler}
+				>
+					+
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default TodoForm;
--- src/components/ZamanBar.js
+++ src/components/ZamanBar.js
@@ -1,42 +1,42 @@
 import React, { useState, useEffect } from "react";
 
 const ZamanBar = () => {
-  // Zamanı göstermek için
-  const Time = () => {
-    const d = new Date();
-    const tarih = d.toLocaleDateString("tr-TR");
-    const saat = d.toLocaleTimeString("tr-TR", {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-    const gun = d.toLocaleString("tr-TR", { weekday: "long" });
-    const hepsi = ` | ${tarih} | ${saat} | ${gun}`;
-    return hepsi;
-  };
+	// Zamanı göstermek için
+	const Time = () => {
+		const d = new Date();
+		const tarih = d.toLocaleDateString("tr-TR");
+		const saat = d.toLocaleTimeString("tr-TR", {
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+		const gun = d.toLocaleString("tr-TR", { weekday: "long" });
+		const hepsi = ` | ${tarih} | ${saat} | ${gun}`;
+		return hepsi;
+	};
 
-  const defaultName = () => {
-    return window.localStorage.getItem("name") || "(ismini girmek için tıkla)";
-    // default name localstorage'da varsa getir yoksa yazıyı göster.
-  };
-  const [name, setname] = useState(defaultName);
+	const defaultName = () => {
+		return window.localStorage.getItem("name") || "(ismini girmek için tıkla)";
+		// default name localstorage'da varsa getir yoksa yazıyı göster.
+	};
+	const [name, setname] = useState(defaultName);
 
-  useEffect(() => {
-    document.title = `${name}'s To Do List`;
-    localStorage.setItem("name", name);
-  });
+	useEffect(() => {
+		document.title = `${name}'s To Do List`;
+		localStorage.setItem("name", name);
+	});
 
-  return (
-    <div className="text-center pb-2">
-      <span
-        className="font-weight-lighter"
-        onClick={() => {
-          setname(prompt("Adın ne şekerlik?"));
-        }}
-      >
-        Merhaba, {name}.
-      </span>
-      {Time()}
-    </div>
-  );
+	return (
+		<div className="text-center pb-2">
+			<span
+				className="font-weight-lighter"
+				onClick={() => {
+					setname(prompt("Adın ne?"));
+				}}
+			>
+				Merhaba, {name}.
+			</span>
+			{Time()}
+		</div>
+	);
 };
 export default ZamanBar;
